071b2de6d5ad625c6b7f31c0575dcc002a0998ce
Saif <639saifulislam@gmail.com>
2026-01-02 15:55:10 +0600

    database configuration update

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd1697d5044f3af4cebc6d4244cddccf464b9f08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e766c0970ef04feac3504587082c299b2473c633")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
